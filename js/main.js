faed913727f8871335667b8703702993cf55051b
Websites-test <melissarosalesmora@gmail.com>
2018-05-30 12:46:43 -0600

    se camabian fondos manchados en el footer y banner

--- js/main.js
+++ js/main.js
@@ -45,3 +45,13 @@ $( document ).ready(function() {
     $(".img-wrapper").height(maxHeight);
 
 });
+
+$(window).ready(function(){
+    $(".boton").wrapInner('<div class=botontext></div>');
+        
+        $(".botontext").clone().appendTo( $(".boton") );
+        
+        $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
+        
+        $(".twist").css("width", "25%").css("width", "+=3px");
+    });
